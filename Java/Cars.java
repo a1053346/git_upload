e85c47f30cfd77f3c8ee7b016f9c968ac2d8ab8d
a1053346 <yyh8778@gmail.com>
2024-08-11 16:56:16 +0800

    Update Cars.java

--- Java/Cars.java
+++ Java/Cars.java
@@ -12,28 +12,58 @@ import java.util.Map;
 public class Cars {
 
 	public static void main(String[] args) {
+
+		//讀檔
+		//整理資料
+			//讀每一行存成string
+				//第一行是欄位名，另存起來，進下一行
+				//第二行起，每一行放進map {欄位名,值}
+				//map放進list
+		//寫檔
+		//匯出
+
+		
 		try (BufferedReader br = new BufferedReader(
 				new FileReader("C:\\Users\\Admin\\Desktop\\Java班\\git_upload\\Java\\cars.csv"))) {
 
 			List<Map<String, String>> resultList = new ArrayList<>();
 
 			String line;
-			while ((line = br.readLine()) != null) {
+			boolean isFirst = true;	//第一次要執行，第二次後改為false
+			List<String> keyList;	//欄位名的list
+			while ((line = br.readLine()) != null) {	//把每一行寫入string
 				System.out.println(line);
-				Map<String, String> map = new HashMap<>();
-				String[] keyValuePairs = line.split(",");
-				for (String keyValuePair : keyValuePairs) {
-					String[] pair = keyValuePair.split(":");
-					if (pair.length == 2) {
-						String key = pair[0].trim();
-						String value = pair[1].trim();
-						map.put(key, value);
-					}
+				
+				if (isFirst) { // 第一行
+					keyList = Arrays.asList(line.split(",")); //第一行是欄位名，另存起來，進下一行
+					isFirst = false; // 註記第一行結束
+				} else { // 第二行起，每一行放進map {欄位名,值}
+					String[] valueArr = line.split(","); // 取得值
+
+					// 遍歷key,建置map
+					Map<String, String> map = new HashMap<>();
+					for (int i=0;i<keyList.length;i++) {
+						map.put(keyList.get(i), valueArr[i]);
+					}					
 				}
-				resultList.add(map);
+				resultList.add(map);	//map放進list
 			}
 			System.out.println(resultList);
 
+			// 用comparator排序price
+			public Comparator<Map<String, String>> mapComparator = new Comparator<Map<String, String>>() {
+    				@Override
+				public int compare(Map<String, String> m1, Map<String, String> m2) {
+        				return m1.get("Price").compareTo(m2.get("Price"));
+    				}
+			}
+			//完成排序
+			Collections.sort(list, mapComparator);	
+
+			//寫檔
+			
+			//匯出
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
